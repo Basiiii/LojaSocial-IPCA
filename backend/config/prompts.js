8335945475a6d9ed8b0e74b9ace06412fed2189f
Enrique R. (Basi) <90209339+Basiiii@users.noreply.github.com>
2026-01-05 21:15:26 +0000

    refactor: update prompt

--- backend/config/prompts.js
+++ backend/config/prompts.js
@@ -15,11 +15,11 @@ const chatSystemPrompt = {
   - P: Qual é o horário de funcionamento? R: Segunda-Sexta 9H-17H
   - P: Como agendar uma entrega? R: Vá para o ecrã inicial e clique em "Faz pedido"
   - P: Quais artigos estão disponíveis? R: Verifique a secção de inventário do aplicativo
-  - P: Quem desenvolveu esta aplicação? R: Enrique R., Diogo Machado, José Alves, Carlos Barreiro
+  - P: Quem desenvolveu esta aplicação? R: Enrique Rodrigues, Diogo Machado, José Alves, Carlos Barreiro
   - P: O que é o IPCA? R: O Politécnico do Cávado e do Ave (IPCA) é uma instituição de ensino superior criada em 1994, com campus em Barcelos e polos em Braga, Guimarães, Famalicão, Esposende e Vila Verde.
   
   Desenvolvimento da Aplicação:
-  Esta aplicação foi desenvolvida por: Enrique R., Diogo Machado, José Alves e Carlos Barreiro.
+  Esta aplicação foi desenvolvida por: Enrique Rodrigues, Diogo Machado, José Alves e Carlos Barreiro.
   
   Sobre o IPCA:
   O Politécnico do Cávado e do Ave (IPCA) foi criado em 1994 e iniciou atividades letivas em 1996/97 com 74 estudantes. Hoje é uma instituição consolidada com 6 escolas superiores e 7.700 estudantes, reconhecida nacional e internacionalmente pela qualidade da sua formação e investigação. O Campus principal em Barcelos ocupa 7 hectares e estende a sua influência a 5 concelhos adicionais. Para 2025 estão previstas novas infraestruturas como o B-CRIC e o Edifício K2D.
